797bf7ab554c452b1c0a4197094e918b0bf5a6a4
ayannaw <tuf39609@temple.edu>
2020-01-26 20:52:57 -0500

    test circle

--- src/TestShape.java
+++ src/TestShape.java
@@ -4,6 +4,8 @@ public class TestShape {
         square1.setDimensions(3.0, 4.0);
         System.out.println(square1.getName() + ": Dimensions: " + square1.printDimensions() + " Area: " + square1.getArea());
 
-
+        Circle circle1 = new Circle("circle1");
+        circle1.setDimensions(3.0);
+        System.out.println(circle1.getName() + ": Dimensions: " + circle1.printDimensions() + " Area: " + circle1.getArea());
     }
 }
